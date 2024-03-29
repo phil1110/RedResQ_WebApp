a24ac657e337b63324d0c35560c29c3f546cdff4
Aron Milojevic <aron.milojevic@edu.szu.at>
2024-03-29 19:18:01 +0100

    An der Page FetchUser weitergearbeitet, und die Methode PromoteUser eingeführt

--- RedResQ_WebApp.Lib/Services/UserService.cs
+++ RedResQ_WebApp.Lib/Services/UserService.cs
@@ -148,5 +148,61 @@ namespace RedResQ_WebApp.Lib.Services
                 return false;
             }
         }
+
+        public async Task<List<Role>> GetRoles()
+        {
+            HttpClient client = Consts.GetHttpClient();
+            string requestUrl = "https://api.redresq.at/role/fetch";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(requestUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Role>? roles = await response.Content.ReadFromJsonAsync<List<Role>>();
+                    return roles ?? new List<Role>(); 
+                }
+                else
+                {
+                    Console.WriteLine($"Error getting roles. Status code: {response.StatusCode}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error getting roles. Message: {ex.Message}");
+            }
+
+            return new List<Role>(); 
+        }
+
+
+        public async Task<bool> PromoteUser(long userId, long roleId)
+        {
+            HttpClient client = Consts.GetHttpClient();
+            string requestUrl = "https://api.redresq.at/user/promote"; 
+
+            var requestData = new { userId, roleId }; 
+
+            try
+            {
+                HttpResponseMessage response = await client.PutAsJsonAsync(requestUrl, requestData);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("User role successfully updated.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"Error promoting user. Status code: {response.StatusCode}");
+                    return false;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error promoting user. Message: {ex.Message}");
+                return false;
+            }
+        }
     }
 }
