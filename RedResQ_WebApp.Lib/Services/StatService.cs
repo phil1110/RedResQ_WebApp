6d4e18a83e5f45cc67abc68398a66fa0d13f6967
Aron Milojevic <aron.milojevic@edu.szu.at>
2024-03-31 23:06:27 +0200

    Letzte Änderungen vor der Abgabe in Italien

---
+++ RedResQ_WebApp.Lib/Services/StatService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedResQ_WebApp.Lib.Services
+{
+	public class StatService
+	{
+
+	}
+}
