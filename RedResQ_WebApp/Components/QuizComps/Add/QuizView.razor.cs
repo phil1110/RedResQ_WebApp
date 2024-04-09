fb862a1a3c7383c1001133264257cf5ac3b1e391
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2024-04-09 20:46:37 +0200

    progress

---
+++ RedResQ_WebApp/Components/QuizComps/Add/QuizView.razor.cs
@@ -0,0 +1,9 @@
+﻿using RedResQ_WebApp.Lib.Models;
+
+namespace RedResQ_WebApp.Components.QuizComps.Add
+{
+    public partial class QuizView
+    {
+        public Quiz? Quiz { get; set; }
+    }
+}
