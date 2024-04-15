0aa8a73cdd5a0d02099bf8a524833296586e8196
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2024-04-15 21:58:11 +0200

    continuing work on the hazard creator

---
+++ RedResQ_WebApp/Components/HazardComps/Add/TypeAdder.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using RedResQ_WebApp.Lib.Services;
+
+namespace RedResQ_WebApp.Components.HazardComps.Add
+{
+    public partial class TypeAdder
+    {
+        private string? TypeName { get; set; }
+
+        [Parameter]
+        public Action AfterAction { get; set; }
+
+        [Parameter]
+        public Action TogglerAction { get; set; }
+
+        private async Task AddType()
+        {
+            await HazardTypeService.Add(TypeName!);
+            AfterAction();
+        }
+    }
+}
--- RedResQ_WebApp/Pages/Hazard/Add.razor.cs
+++ RedResQ_WebApp/Pages/Hazard/Add.razor.cs
@@ -1,6 +1,15 @@
-﻿namespace RedResQ_WebApp.Pages.Hazard
+﻿using RedResQ_WebApp.Lib.Models;
+using RedResQ_WebApp.Lib;
+using RedResQ_WebApp.Lib.Services;
+
+namespace RedResQ_WebApp.Pages.Hazard
 {
     public partial class Add
     {
+
+        public void SubmitHazard()
+        {
+            // title, lat, lon, radius, typeId
+        }
     }
 }
