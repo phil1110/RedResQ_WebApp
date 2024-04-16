ef799bb7124865431d060df9075524512edb40bd
Aron Milojevic <aron.milojevic@edu.szu.at>
2024-04-16 16:13:38 +0200

    Kleine Änderungen Vorgenommen

--- RedResQ_WebApp.Lib/Services/StatService.cs
+++ RedResQ_WebApp.Lib/Services/StatService.cs
@@ -1,13 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
+using RedResQ_WebApp.Lib.Models;
 
 namespace RedResQ_WebApp.Lib.Services
 {
 	public class StatService
 	{
+		public static async Task<string[]> GetStatTypes()
+		{
+			var client = Consts.GetHttpClient();
+			PathBuilder pathBuilder = new PathBuilder("stattype");
 
+			try
+			{
+				HttpResponseMessage response = await client.GetAsync(pathBuilder.ToString());
+				if (response.IsSuccessStatusCode)
+				{
+					return await response.Content.ReadFromJsonAsync<string[]>();
+				}
+				else
+				{
+					Console.WriteLine($"Error getting quiz types. Status code: {response.StatusCode}");
+				}
+			}
+			catch (HttpRequestException ex)
+			{
+				Console.WriteLine($"Error getting quiz types. Message: {ex.Message}");
+			}
+
+			return null!;
+		}
+
+		public static async Task<Dictionary<string, long>> GetStatByType(string statType)
+		{
+			var client = Consts.GetHttpClient();
+			PathBuilder pathBuilder = new PathBuilder("stat");
+			pathBuilder.AddParameter("statName", statType);
+			// string , long 
+
+			try
+			{
+				HttpResponseMessage response = await client.GetAsync(pathBuilder.ToString());
+				if (response.IsSuccessStatusCode)
+				{
+					return await response.Content.ReadFromJsonAsync<Dictionary<string, long>>();
+				}
+				else
+				{
+					Console.WriteLine($"Error getting quiz by type '{statType}'. Status code: {response.StatusCode}");
+				}
+			}
+			catch (HttpRequestException ex)
+			{
+				Console.WriteLine($"Error getting quiz by type '{statType}'. Message: {ex.Message}");
+			}
+
+			return null!;
+		}
 	}
 }
